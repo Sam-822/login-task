71eca312569e1d4f68a48f61dda5496973c13a82
AbdulSamad <ansamad1028@gmail.com>
2025-06-10 14:37:48 +0530

    added hash locations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
 import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
 	{
 		path:'',
-		component: HomePageComponent,
+		component: LoginPageComponent
 	},
 	{
 		path:'login',
